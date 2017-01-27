7758d59645d41894d8f16f12081cbf105ad8ae46
Neeraj Shukla <neerajshukla1911@gmail.com>
2017-01-27 11:19:47 +0530

    changed path

--- predict.py
+++ predict.py
@@ -4,9 +4,9 @@ import h5py
 
 model = AlexNet().create([None, 128, 128, 3], 4)
 
-model.load('trained_models/alexnet/fynd/my_model.tflearn', weights_only=True)
+model.load('trained_models/alexnet/my_model.tflearn', weights_only=True)
 
-img = data_utils.load_image('/home/neeraj/AI/aml_data/fynd/2.jpg')
+img = data_utils.load_image('/home/neeraj/AI/aml_data/2.jpg')
 img = data_utils.resize_image(img, 128, 128)
 img = data_utils.pil_to_nparray(img)
 print(model.predict_label([img]))
