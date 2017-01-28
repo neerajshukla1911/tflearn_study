b8cb286aec3133e21fc7f20b929cc174cbe9340b
Neeraj Shukla <neerajshukla1911@gmail.com>
2017-01-28 13:35:01 +0530

    bug fix2

--- predict.py
+++ predict.py
@@ -8,12 +8,11 @@ model = AlexNet().create([None, 128, 128, 3], 4)
 
 model.load('trained_models/alexnet/my_model.tflearn', weights_only=True)
 
-img = data_utils.load_image('{}/{}'.format( os.getenv("HOME"),'AI/data/2.jpg')
+img = data_utils.load_image('{}/{}'.format( os.getenv("HOME"),'AI/data/2.jpg'))
 img = data_utils.resize_image(img, 128, 128)
 img = data_utils.pil_to_nparray(img)
 print(model.predict_label([img]))
 
-h5f = h5py.File('{}/{}'.format( os.getenv("HOME"),'AI/data/fynd/dataset.h5'), 'r')
 #load trained model and predict labels
 # model = VGG16().create([None, 224, 224, 3], 4)
 # model = VGG16().create([None, 224, 224, 3], 4)
